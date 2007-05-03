066e043129698cc326df55cb5117ec72cbe49874
Sune Vuorela <sune@debian.org>
2007-05-03 22:05:06 +0000

    mm... echo "include debian/cdbs/kde.mk" > debian/rules ; debuild This should be svn:externals. Wonder if it works

---
+++ kde.mk
@@ -0,0 +1,20 @@
+include debian/cdbs/cmake.mk
+include /usr/share/cdbs/1/rules/debhelper.mk
+include /usr/share/cdbs/1/rules/patchsys-quilt.mk
+include /usr/share/cdbs/1/rules/utils.mk
+
+DEB_DH_INSTALL_ARGS = --sourcedir=debian/tmp
+DEB_COMPRESS_EXCLUDE = .dcl .docbook -license .tag .sty .el
+
+
+DEB_CMAKE_EXTRA_FLAGS = $(DEB_CMAKE_EXTRA_FLAGS) -D
+
+ifeq (,$(findstring noopt,$(DEB_BUILD_OPTIONS)))
+	#no optimizations, full debug
+       DEB_CMAKE_DEBUG_FLAGS = -DCMAKE_BUILD_TYPE=debugfull
+else
+	#This is around -O2 -g
+       DEB_CMAKE_DEBUG_FLAGS = -DCMAKE_BUILD_TYPE=relwithdebinfo
+endif
+
+
