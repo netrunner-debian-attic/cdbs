dd81781a70c36ff5cd7de41d33336fcdfe3d7c0d
Modestas Vainius <modax@debian.org>
2008-07-12 15:11:53 +0000

    This syntax might be dangerous (self reference)

--- kde.mk
+++ kde.mk
@@ -7,7 +7,7 @@ include /usr/share/cdbs/1/rules/utils.mk
 include debian/cdbs/variables.mk
 # Pass standard KDE 4 flags to cmake via appropriate CDBS variable
 # (DEB_CMAKE_EXTRA_FLAGS)
-DEB_CMAKE_EXTRA_FLAGS = $(DEB_CMAKE_KDE4_FLAGS) $(DEB_CMAKE_CUSTOM_FLAGS) $(DEB_CMAKE_EXTRA_FLAGS)
+DEB_CMAKE_EXTRA_FLAGS += $(DEB_CMAKE_KDE4_FLAGS) $(DEB_CMAKE_CUSTOM_FLAGS)
 
 DEB_COMPRESS_EXCLUDE = .dcl .docbook -license .tag .sty .el
 
